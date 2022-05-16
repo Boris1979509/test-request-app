48193caceac12d4c5462d42f281cfdc1adf3d308
Boris <bobabonanadz@mail.ru>
2022-05-16 19:55:13 +0300

    Added request seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RequestSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
